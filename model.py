b686bb3340aa8203c5eb1370a7c025e54d9b99af
dominik098 <44304731+dominik098@users.noreply.github.com>
2020-03-16 22:07:08 +0100

    Add files via upload
    Wprowadzony cały model z animacją. Brakuje uchybu sterowania (zamkniętej pętli sterowania, koniecznie trzeba ją dodać). Zmienne uchybu sterowania są tylko zdefiniowane.

--- model.py
+++ model.py
@@ -8,14 +8,17 @@ import matplotlib.animation as animation
 G = 9.8 # przyspieszenie ziemskie
 L1 = 1.0 # długość członu pierwszego
 L2 = 1.0 # długość członu drugiego
-M1 = 1.0 # masa członu pierwszego
+M1 = 0.5 # masa członu pierwszego
 M2 = 1.0 # masa członu drugiego
 Kp = 1.0 # wzmocnienie członu Kp 
 
-th1_ = 0.0 # wartość zadana kąta przegubu pierwszego (stopnie)
-th2_ = 0.0 # wartość zadana kąta przegubu drugiego (stopnie)
+th1_ = 12 # wartość zadana kąta przegubu pierwszego (stopnie)
+th2_ = 7 # wartość zadana kąta przegubu drugiego (stopnie)
 
-def derives(state, t):
+e1 = 0.0 # wartosc uchybu regulacji przegubu pierwszego
+e2 = 0.0 # wartosc uchybu regulacji przegubu drugiego
+
+def derivs(state, t):
 
     dydx = np. zeros_like(state)
 
@@ -36,22 +39,18 @@ def derives(state, t):
     
     dydx[0] = state[1]
 
-    dydx[1] = ( + M22*(Kp*th1_ - C12*state[3]
-                - G11) - M12*(Kp*th2_
-                - C21*state[1] - G21))/den)
+    dydx[1] = ((+ M22*(Kp*th1_ - C12*state[3] - G11) - M12*(Kp*th2_ - C21*state[1] - G21))/den)
     
     dydx[2] = state[3]
 
-    dydx[3] = ( - M21*(Kp*th1_ - C12*state[3]
-                - G11) + M11(Kp*th2_
-                - C21*state[1] - G21)/den)
+    dydx[3] = ((- M21*(Kp*th1_ - C12*state[3] - G11) + M11*(Kp*th2_ - C21*state[1] - G21))/den)
     
     return dydx
 
 
 # tworzenie tablicy czasu od 0..100 próbkowanej co 0.05 sekundy
 dt = 0.05
-t = np.arrange(0, 20, dt)
+t = np.arange(0, 20, dt)
 
 # th1 i th2 są początkowymi kątami (stopnie)
 # w1 i w2 są początkowymi prędkościami kątowymi (stopnie na sekundę)
@@ -62,3 +61,41 @@ w2 = 0.0
 
 # stan początkowy
 state = np.radians([th1, w1, th2, w2])
+
+# integrate your ODE using scipy.integrate.
+y = integrate.odeint(derivs, state, t)
+
+x1 = L1*sin(y[:, 0])
+y1 = -L1*cos(y[:, 0])
+
+x2 = L2*sin(y[:, 2]) + x1
+y2 = -L2*cos(y[:, 2]) + y1
+
+fig = plt.figure()
+ax = fig.add_subplot(111, autoscale_on=False, xlim=(-2, 2), ylim=(-2, 2))
+ax.set_aspect('equal')
+ax.grid()
+
+line, = ax.plot([], [], 'o-', lw=2)
+time_template = 'time = %.1fs'
+time_text = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+
+
+def init():
+    line.set_data([], [])
+    time_text.set_text('')
+    return line, time_text
+
+
+def animate(i):
+    thisx = [0, x1[i], x2[i]]
+    thisy = [0, y1[i], y2[i]]
+
+    line.set_data(thisx, thisy)
+    time_text.set_text(time_template % (i*dt))
+    return line, time_text
+
+
+ani = animation.FuncAnimation(fig, animate, range(1, len(y)),
+                              interval=dt*1000, blit=True, init_func=init)
+plt.show()
